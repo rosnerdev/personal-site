92698bec7f86977278586ff85e1f90dae1074fc3
rosnerdev <yrosner123@gmail.com>
2022-09-27 18:31:27 +0300

    added a terminal page and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+const NavLinks: string[][] = [['home','/'], ['terminal','/terminal']];
+
+const Navbar = () => {
+    return (
+        <div className="fixed text-xl my-4 w-full">
+            <div className="flex justify-center gap-4">
+                {NavLinks.map((link) => {
+                    return <a href={link[1]} className="dark:text-white hover:underline">{link[0]}</a>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
