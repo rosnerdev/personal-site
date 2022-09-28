66162172f7a37e308e3853214d61d4b3320befb9
rosnerdev <yrosner123@gmail.com>
2022-09-28 06:45:17 +0300

    fixed a few things

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,11 @@ const Navbar = () => {
         <div className="fixed text-xl my-4 w-full">
             <div className="flex justify-center gap-4">
                 {NavLinks.map((link) => {
-                    return <a href={link[1]} className="dark:text-white hover:underline">{link[0]}</a>
+                    return (
+                        <a href={link[1]} key={link[0]} className="dark:text-white hover:underline">
+                            {link[0]}
+                        </a>
+                    )
                 })}
             </div>
         </div>
